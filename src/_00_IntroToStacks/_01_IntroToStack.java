806f263b3f825f28cf21465e49a7e3c234c8aa8b
league <league@imac-11.attlocal.net>
2022-08-01 16:58:39 -0700

    finished module 3 now module4

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,13 +9,31 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+Stack<Double> digits = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+for (int i = 0; i < 100; i++) {
+	Random ran = new Random();
+	double rand = ran.nextDouble();
+	rand = rand * 100;
+	digits.push(rand);
+	System.out.println(rand);
+}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+String low = JOptionPane.showInputDialog("Enter in lowest number: ");
+String high = JOptionPane.showInputDialog("Enter in highest number: ");
+
+int lowest = Integer.parseInt(low);
+int highest = Integer.parseInt(high);
 
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
+while( ! digits.isEmpty() ) {
+    double popped = digits.pop();
+    if (popped > lowest && popped < highest) {
+		System.out.println(popped);
+	}
+}
+
 
 
         // EXAMPLE:
