47370149edb37926d4951a51eefa1effe3b9268e
league <league@imac-18.attlocal.net>
2022-08-29 18:29:13 -0700

    finioshed

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,7 @@
 package _03_Hangman;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,38 +13,85 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.xml.stream.events.Characters;
 
-public class Hangman {
+public class Hangman implements KeyListener {
 
-	public static void main(String[] args) {
-		
-		JFrame frame = new JFrame("HangMan");
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel();
-		
+	JFrame frame = new JFrame("HangMan");
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	String secrets = null;
+	char[] secret;
+	char[] actual;
+	String temp2;
+	String word;
+
+	void run() {
+
+		frame.setVisible(true);
 		int lives = 10;
-		
+
 		frame.add(panel);
 		panel.add(label);
-		
-		String secrets = null;
-		
+
+		frame.addKeyListener(this);
+
 		String question = JOptionPane.showInputDialog("Input number");
 		int inputted = Integer.parseInt(question);
-		
-		String word = Utilities.readRandomLineFromFile("dictionary.txt");
+
+		word = Utilities.readRandomLineFromFile("dictionary.txt");
 		char[] chars = word.toCharArray();
-		
-		char[] secret = chars;
+		// System.out.println(word);
+		actual = chars.clone();
+		System.out.println(actual);
+		secret = chars;
+		System.out.println(chars);
+		// System.out.println(secret[1]);
+
+		StringBuilder empty = new StringBuilder();
+
 		for (int i = 0; i < secret.length; i++) {
 			secret[i] = '_';
-			 secrets = secret.toString();
+			empty.append('_');
+
 		}
-		label.setText(secrets);
+		String constructed = empty.toString();
+		label.setText(constructed);
+		// System.out.println(constructed);
+		frame.pack();
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char pressed = e.getKeyChar();
+		System.out.println(pressed);
+		//System.out.println(actual);
+		System.out.println(secret);
+		String temps = null;
+		for (int i = 0; i < secret.length; i++) {
+			if (actual[i] == pressed) {
+				secret[i] = pressed;
+			}
+			
+
+		}
+		label.setText(new String(secret));
+		System.out.println(label.getText());
 		
-		
-		
-		
-		
-		
+		if (label.getText().equals(word)) {
+			JOptionPane.showMessageDialog(null, "You win the game");
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
---
+++ src/_03_Hangman/HangmanRunner.java
@@ -0,0 +1,11 @@
+package _03_Hangman;
+
+public class HangmanRunner {
+
+	public static void main(String[] args) {
+		new Hangman().run();
+		
+
+		
+	}
+}
